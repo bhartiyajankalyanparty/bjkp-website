c53c5d7991f842d759a448804564e505139f99ef
bhartiyajankalyanparty <info.bjkp.org.in@gmail.com>
2026-07-30 21:08:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ document.addEventListener("DOMContentLoaded", function () {
         alert("जल्द ही ऑनलाइन सदस्यता फॉर्म उपलब्ध होगा।");
     });
 });
+// Image Slider
+const images = [
+  "image/banner1.jpg",
+  "image/banner2.jpg",
+  "image/banner3.jpg"
+];
+
+let current = 0;
+
+setInterval(() => {
+  current = (current + 1) % images.length;
+  const slider = document.getElementById("sliderImage");
+  if (slider) {
+    slider.src = images[current];
+  }
+}, 3000);
