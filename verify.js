5c927fbb28a6c324ec2fc825506e00b35531d1cd
bhartiyajankalyanparty <info.bjkp.org.in@gmail.com>
2026-08-02 04:51:26 +0530

    Update verify.js

--- verify.js
+++ verify.js
@@ -9,7 +9,46 @@ import {
 const firebaseConfig = {
   apiKey: "AIzaSyBGa257kKYT4zJcUSyeu7aITZ0Y3D6AYk0",
   authDomain: "bhartiya-jan-kalyan-party-org.firebaseapp.com",
+  projectId: "bhartiya-jan-kalyanimport { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGa257kKYT4zJcUSyeu7aITZ0Y3D6AYk0",
+  authDomain: "bhartiya-jan-kalyan-party-org.firebaseapp.com",
   projectId: "bhartiya-jan-kalyan-party-org",
+  storageBucket: "bhartiya-jan-kalyan-party-org.firebasestorage.app",
+  messagingSenderId: "715864126578",
+  appId: "1:715864126578:web:9f9901e4c2a119b225beeb"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.verifyMember = async function () {
+
+  const memberId = document.getElementById("memberId").value.trim();
+
+  if (memberId === "") {
+    alert("Member ID दर्ज करें");
+    return;
+  }
+
+  const snapshot = await getDocs(collection(db, "members"));
+
+  let found = false;
+
+  snapshot.forEach((doc) => {
+
+    const m = doc.data();
+
+    if (m.memberId === memberId) {
+
+      found-org",
   storageBucket: "bhartiya-jan-kalyan-party-org.firebasestorage.app",
   messagingSenderId: "715864126578",
   appId: "1:715864126578:web:9f9901e4c2a119b225beeb"
