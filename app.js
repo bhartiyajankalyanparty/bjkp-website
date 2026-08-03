6dbdb98dd1596dff897942892720a5cab6a3f81d
bhartiyajankalyanparty <info.bjkp.org.in@gmail.com>
2026-08-03 11:45:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -231,3 +231,29 @@ document.addEventListener("DOMContentLoaded",()=>{
 console.log("🚀 Home Page Ready");
 
 });
+
+/* ===== Hero Slider ===== */
+
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+if(slides.length > 0){
+
+setInterval(()=>{
+
+slides[currentSlide].classList.remove("active");
+
+currentSlide++;
+
+if(currentSlide >= slides.length){
+
+currentSlide = 0;
+
+}
+
+slides[currentSlide].classList.add("active");
+
+},4000);
+
+}
