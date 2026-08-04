24dea0235921444e86babf116c1522d8877825a2
bhartiyajankalyanparty <info.bjkp.org.in@gmail.com>
2026-08-04 20:00:38 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-import {
-  getFirestore,
-  collection,
-  addDoc
-} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBGa257kKYT4zJcUSyeu7aITZ0Y3D6AYk0",
@@ -17,14 +13,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-window.saveMember = async function(name, mobile, email, address) {
-  await addDoc(collection(db, "members"), {
-    name,
-    mobile,
-    email,
-    address,
-    createdAt: new Date()
-  });
-
-  alert("सदस्यता सफलतापूर्वक दर्ज हो गई।");
-};
+export { app, db };
