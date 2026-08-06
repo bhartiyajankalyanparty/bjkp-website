3683c3de26273b7e69c75e8b66f6d760300bea47
bhartiyajankalyanparty <info.bjkp.org.in@gmail.com>
2026-08-06 11:54:28 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,8 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-storage.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBIq9s8Ix225swy8U8KAxTYduGcBcMoQQQ",
   authDomain: "bjkp-official-website.firebaseapp.com",
@@ -8,3 +12,11 @@ const firebaseConfig = {
   appId: "1:640178809634:web:a0b1f0665037ad3844d37b",
   measurementId: "G-VZ01W7XJRS"
 };
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
