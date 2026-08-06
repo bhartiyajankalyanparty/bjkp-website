2b1052388cfbe0a125b760ddd73a1384fb8cc8eb
bhartiyajankalyanparty <info.bjkp.org.in@gmail.com>
2026-08-06 11:22:08 +0530

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBIq9s8Ix225swy8U8KAxTYduGcBcMoQQQ",
+  authDomain: "bjkp-official-website.firebaseapp.com",
+  projectId: "bjkp-official-website",
+  storageBucket: "bjkp-official-website.firebasestorage.app",
+  messagingSenderId: "640178809634",
+  appId: "1:640178809634:web:a0b1f0665037ad3844d37b",
+  measurementId: "G-VZ01W7XJRS"
+};
